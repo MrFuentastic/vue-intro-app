4810f4ddbcc52a94d5932befe71abdae059f6e12
MrFuentastic <vfuentes8587@gmail.com>
2017-10-17 12:15:03 -0500

    initial commit

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  var app = new Vue({
+    el: '#app',
+    data: {
+      message: "Hey Vue, how's it going?"
+    },
+    mounted: function() {
+
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+  });
+});
